b39d9a0b586f301e108df3bc977ed80715606004
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-04-23 17:28:46 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,21 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Main from './components/Main';
+import { RecoilRoot } from 'recoil';
+import RecoilNexus from 'recoil-nexus';
 
 function App() {
 
 
-  return <Routes>
-    <Route path="/login" element={<Login />} />
-    <Route path="/" element={<Main />} />
-  </Routes>
+  return (
+    <RecoilRoot>
+      <RecoilNexus />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </RecoilRoot>
+  )
 }
 
 export default App;
