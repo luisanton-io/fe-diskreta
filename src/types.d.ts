91f6366c59c9c147b1466c34aa3388195fe67bdd
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-04-27 01:21:05 +0400

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,27 +1,10 @@
-
-interface User {
-    id: string;
-    username: string;
-    pubkey: string;
-}
-
-interface Message {
-    sender: User["id"]
-    recipients: User[]
-    chatId: string
-    content: {
-        text: string;
-        media?: string
-    }
-    timestamp: number
-}
-
-interface Chat {
-    messages: Message[];
-    members: User[]
-}
-
 interface Store {
     chats: Record<string, Chat>
     active: null | string
+}
+
+interface Dialog {
+    Content: () => JSX.Element
+    submitLabel: string | null
+    onClose: () => void
 }
