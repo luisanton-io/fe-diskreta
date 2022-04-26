89e92051e49fd38fe3ff8761237f00983510d315
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-04-27 01:20:39 +0400

    Create persist.ts

---
+++ src/atoms/effects/persist.ts
@@ -0,0 +1,46 @@
+import { AES, enc } from "crypto-js"
+import { AtomEffect } from "recoil"
+import { getRecoil } from "recoil-nexus"
+import { recoilPersist } from "recoil-persist"
+import { userState } from "../user"
+
+const encrypt = (message: string) => {
+    try {
+        const value = AES.encrypt(message, getRecoil(userState)?.digest ?? '').toString()
+        console.table({ value, id: getRecoil(userState)?.digest, message })
+        return value
+    } catch {
+        return null
+    }
+}
+
+const decrypt = (cipher: string) => {
+    try {
+        const value = AES.decrypt(cipher, getRecoil(userState)?.digest ?? '').toString(enc.Utf8)
+        console.table({ value })
+        return value
+    } catch {
+        return null
+    }
+}
+
+export const encryptedStorage = () => {
+    return {
+        setItem: (key: string, value: string) => {
+            console.table({ key, value })
+            localStorage.setItem(key, encrypt(value) as string)
+        },
+        getItem: (key: string) => {
+            const a = localStorage.getItem(key)
+            console.table({ decrypted: a && decrypt(a) })
+            return a && decrypt(a)
+        },
+        clear: () => {
+            localStorage.clear()
+        },
+    }
+}
+
+const persist = (key: string): { persistAtom: AtomEffect<any> } => { return recoilPersist({ key, storage: encryptedStorage() }) }
+
+export default persist
