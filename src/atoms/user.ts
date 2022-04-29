6405bb05a593253a1b111b1f06599145d86f7140
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-04-30 00:13:51 +0400

    Update user.ts

--- src/atoms/user.ts
+++ src/atoms/user.ts
@@ -3,12 +3,6 @@ import persist from "./effects/persist";
 
 const { persistAtom } = persist("user");
 
-interface LoggedUser extends User {
-    token: string
-    privateKey: string
-    digest: string
-}
-
 export const userState = atom<LoggedUser | null>({
     key: "user",
     default: null,
