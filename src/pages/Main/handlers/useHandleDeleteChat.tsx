a188e8d245b3d535b5663ef82cb439d5dba3ba18
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-05-16 14:03:25 +0400

    Navigate after chat deletion

--- src/pages/Main/handlers/useHandleDeleteChat.tsx
+++ src/pages/Main/handlers/useHandleDeleteChat.tsx
@@ -1,14 +1,17 @@
 import { chatsState } from "atoms/chats"
+import { useNavigate } from "react-router-dom"
 import { useRecoilState } from "recoil"
 
 export default function useHandleDeleteChat() {
     const [chats, setChats] = useRecoilState(chatsState)
+    const navigate = useNavigate()
 
     return (id: string) => {
         if (window.confirm("Are you sure you want to delete this chat?")) {
             const _chats = { ...chats }
             delete _chats![id]
             setChats({ ..._chats })
+            navigate("/")
         }
     }
 }
