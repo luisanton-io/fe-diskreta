a445254e55e693388beabf0163b9ed071a1faafa
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-05-17 11:57:08 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { AES, enc, SHA512 } from 'crypto-js';
 import { md, pki } from 'node-forge';
 import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { RecoilRoot } from 'recoil';
 import RecoilNexus from 'recoil-nexus';
-import './App.scss';
 import Dialog from './components/Dialog';
 import Login from './pages/Login';
 import Main from './pages/Main';
 import Register from './pages/Register';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './styles/index.scss';
 
 (window as any).AES = AES;
 (window as any).pki = pki;
