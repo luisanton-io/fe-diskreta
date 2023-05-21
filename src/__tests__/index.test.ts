d3e4160e5faaa064330e0599398c116c102191f8
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2023-05-21 22:46:27 +0400

    Update index.test.ts

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,26 +1,24 @@
-import { generateMnemonic, mnemonicToSeed } from "bip39";
-import { pki, random } from "node-forge";
+import { generateMnemonic } from "bip39";
+import { pki } from "node-forge";
 import generateKeyPair from "util/generateKeypair";
 
 jest.setTimeout(1000000)
 
 describe("Deterministic key generation", () => {
 
-    const mnemonic = generateMnemonic(256)
+    it("should generate RSA pairs deterministically and consistently from the same mnemonic", async () => {
 
-    it("should generate RSA pairs deterministically from mnemonic", async () => {
-        const seed = (await mnemonicToSeed(mnemonic)).toString('hex')
+        const mnemonic = generateMnemonic(256)
 
-        const prng = random.createInstance();
-        prng.seedFileSync = () => seed
-
-        const keys = Array(2).fill(
+        // Generate 10 keys from the same mnemonic
+        const keys = Array(10).fill(
             pki.publicKeyToPem(
                 (await generateKeyPair(mnemonic)).publicKey
             )
         )
 
-        expect(keys[0]).toEqual(keys[1])
+        // All keys should be the same
+        expect(keys.every(key => key === keys[0])).toBe(true)
 
     })
 })
